60c83cf2b9d532f86de19eac63dbf5fb919a1a0e
Cedric <cmer@cmer.fr>
2023-11-05 21:58:26 +0100

    Update

--- scripts/install-ohmyzsh.sh
+++ scripts/install-ohmyzsh.sh
@@ -1,73 +1,60 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
-# Stop execution if any command fails
-set -e
+# Set necessary variables
+OH_MY_ZSH_DIR="$HOME/.oh-my-zsh"
+OH_MY_ZSH_INSTALL_SCRIPT="https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh"
+POWERLEVEL10K_DIR="${ZSH_CUSTOM:-$OH_MY_ZSH_DIR/custom}/themes/powerlevel10k"
+AUTOSUGGESTIONS_DIR="${ZSH_CUSTOM:-$OH_MY_ZSH_DIR/custom}/plugins/zsh-autosuggestions"
+SYNTAX_HIGHLIGHTING_DIR="${ZSH_CUSTOM:-$OH_MY_ZSH_DIR/custom}/plugins/zsh-syntax-highlighting"
 
-# Define the installation directory for oh-my-zsh
-DIR="$HOME/.oh-my-zsh"
-
-# Function to install oh-my-zsh
-install_oh_my_zsh() {
-  echo "Installing oh-my-zsh..."
-  sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+# Function to check if a command exists
+command_exists() {
+  command -v "$@" >/dev/null 2>&1
 }
 
-# Function to install a theme
-install_theme() {
-  local theme_name="$1"
-  local theme_url="$2"
-  local theme_path="${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/$theme_name"
-
-  # Remove the existing theme directory if it exists to ensure a fresh install
-  sudo rm -rf "$theme_path"
-  git clone --depth=1 "$theme_url" "$theme_path"
-  
-  # Update the .zshrc file to use the new theme
-  sed -i "s|^ZSH_THEME=.*|ZSH_THEME=\"$theme_name/$theme_name\"|g" "$HOME/.zshrc"
-}
-
-# Function to install plugins
-install_plugins() {
-  local plugins=("$@")
-  for plugin in "${plugins[@]}"; do
-    local plugin_url="https://github.com/zsh-users/${plugin}.git"
-    local plugin_path="${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/plugins/${plugin}"
-
-    # Clone plugin repository
-    git clone "$plugin_url" "$plugin_path"
-  done
-}
-
-# Function to enable plugins in .zshrc
-enable_plugins() {
-  local plugins_string="plugins=($@)"
-  sed -i "s|^plugins=(.*)|$plugins_string|g" "$HOME/.zshrc"
-}
+# Ensure necessary tools are installed
+for tool in curl git apt; do
+  if ! command_exists $tool; then
+    echo "Error: $tool is not installed." >&2
+    exit 1
+  fi
+done
 
 # Check if oh-my-zsh is already installed
-if [ -d "$DIR" ]; then
+if [ -d "$OH_MY_ZSH_DIR" ]; then
   echo "oh-my-zsh is already installed. Skipping..."
 else
-  install_oh_my_zsh
+  echo "Installing oh-my-zsh..."
+  # Install oh-my-zsh
+  sh -c "$(curl -fsSL $OH_MY_ZSH_INSTALL_SCRIPT)"
 
-  # Install the powerline fonts
-  sudo apt-get install fonts-powerline -y
+  # Check for successful installation
+  if [ ! -d "$OH_MY_ZSH_DIR" ]; then
+    echo "Installation of oh-my-zsh failed." >&2
+    exit 1
+  fi
 
-  # Install and configure powerlevel10k theme
-  install_theme "powerlevel10k" "https://github.com/romkatv/powerlevel10k.git"
+  # Install fonts-powerline
+  sudo apt install fonts-powerline -y
+
+  # Install and configure powerlevel10k
+  sudo rm -rf "$POWERLEVEL10K_DIR"
+  git clone --depth=1 https://github.com/romkatv/powerlevel10k.git "$POWERLEVEL10K_DIR"
+  sed -i 's/ZSH_THEME=".*"/ZSH_THEME="powerlevel10k/powerlevel10k"/g' ~/.zshrc
 
   # Install plugins
-  install_plugins "zsh-autosuggestions" "zsh-syntax-highlighting"
+  git clone https://github.com/zsh-users/zsh-autosuggestions.git "$AUTOSUGGESTIONS_DIR"
+  git clone https://github.com/zsh-users/zsh-syntax-highlighting.git "$SYNTAX_HIGHLIGHTING_DIR"
 
-  # Enable plugins (including the ones we just installed)
-  enable_plugins git zsh-autosuggestions zsh-syntax-highlighting kubectl kube-ps1
+  # Enable plugins
+  sed -i 's/plugins=(.*)/plugins=(git zsh-autosuggestions zsh-syntax-highlighting)/g' ~/.zshrc
+  
+  # Disable auto wizard
+  echo "POWERLEVEL9K_DISABLE_CONFIGURATION_WIZARD=true" >> ~/.zshrc
 
-  # Disable the automatic configuration wizard for powerlevel10k
-  echo "POWERLEVEL9K_DISABLE_CONFIGURATION_WIZARD=true" >> "$HOME/.zshrc"
-
-  # Optionally, force zsh as the default shell
-  echo "exec zsh" >> "$HOME/.bashrc"
-
-  echo "Successfully Installed. Start new terminal and configure powerlevel10k."
-  echo "Thanks for using this script. It actually saves lot of time to install & configure zsh."
+  # Suggest to start a new zsh session
+  echo "Please start a new zsh session or source ~/.zshrc to apply changes."
 fi
+
+# Final message
+echo "Thanks for using this script. It saves a lot of time to install & configure zsh."
