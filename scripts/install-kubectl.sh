e9b66afa43a32aba4fb14f46d07d4d05e1fd31f3
Cedric <cmer@cmer.fr>
2023-11-05 18:06:03 +0100

    update

--- scripts/install-kubectl.sh
+++ scripts/install-kubectl.sh
@@ -6,6 +6,7 @@ for pkg in $pkgs; do
   status="$(dpkg-query -W --showformat='${db:Status-Status}' "$pkg" 2>&1)"
   if [ ! $? = 0 ] || [ ! "$status" = installed ]; then
     install=true
+    echo "$pkgs already installed"
     break
   fi
 done
@@ -14,5 +15,6 @@ if "$install"; then
   curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add -
   echo "deb https://apt.kubernetes.io/ kubernetes-xenial main" | sudo tee -a /etc/apt/sources.list.d/kubernetes.list
   sudo apt-get update
-  sudo apt-get install -y kubectl
+  sudo apt-get install -y $pkgs
+  echo "successful installation for $pkgs"
 fi
