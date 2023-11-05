1a542231a7b97ae8457495daeff64ba044bd95ea
Cedric <cmer@cmer.fr>
2023-11-05 22:41:37 +0100

    Update

--- install.sh
+++ install.sh
@@ -1,21 +1,35 @@
 #!/bin/bash
+set -e # Stop the script in case of an error
 
+# Set the locale
 sudo locale-gen en_US.UTF-8
 sudo dpkg-reconfigure --frontend=noninteractive locales
 
+# Install zsh
+sudo apt update && sudo apt install zsh -y
 
-# Install zsh and apt-utils
-sudo apt install zsh apt-utils -y
+# Set zsh as the default shell
+if ! sudo chsh -s /usr/bin/zsh; then
+    echo "Error changing the default shell to zsh"
+    exit 1
+fi
 
-# Show version
-zsh --version
+# Function to execute installation scripts
+run_script() {
+    script_path="./scripts/$1.sh"
 
-# Set zsh as default shell
-sudo chsh -s /usr/bin/zsh 
+    if [[ -x "$script_path" ]]; then
+        echo "Executing script $script_path"
+        sh "$script_path" || { echo "Error executing $script_path"; exit 1; }
+    else
+        echo "The script $script_path does not exist or is not executable."
+        exit 1
+    fi
+}
 
+# Install Oh My Zsh, kubectl, and gcloud
+run_script "install-ohmyzsh"
+run_script "install-kubectl"
+run_script "install-gcloud"
 
-# Install Script
-
-sh ./scripts/install-ohmyzsh.sh
-# sh ./scripts/install-kubectl.sh
-# sh ./scripts/install-gcloud.sh
+echo "Installation completed successfully."
